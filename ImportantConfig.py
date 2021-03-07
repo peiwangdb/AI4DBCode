1aec5bde78d473cd587cf064232e6f23a68c3592
anonymous-peiw <peiw.huawei@gmail.com>
2021-03-07 10:29:25 -0800

    modify postgress config info

--- ImportantConfig.py
+++ ImportantConfig.py
@@ -1,11 +1,10 @@
 class Config:
     def __init__(self,):
         self.sytheticDir = "Queries/sytheic"
-        self.JOBDir = "Queries/JOB"
+        self.JOBDir = "JOB-queries"
         self.schemaFile = "schema.sql"
-        self.dbName = ""
-        self.userName = ""
-        self.password = ""
+        self.dbName = "imdbload"
+        self.userName = "peiw"
+        self.password = "123456"
         self.ip = "127.0.0.1"
         self.port = 5432
-
